e392fb2e2c9ae90e87edfc229823720dd2379b2e
Matthias Chen <matthias_cy@outlook.com>
2016-01-19 17:37:14 +0800

    bone

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="Phlox",
+      version="0.1.0",
+      description="Python Computer Version Library",
+      author="Matthias Chen",
+      author_email="matthias_cy@outlook.com",
+      url="http://jardinier.github.io/phlox",
+      packages=['phlox'])
