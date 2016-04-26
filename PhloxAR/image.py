66ef963613fdd0bac89915ab478cf87b0fc515aa
Matthias Chen <matthias_cy@outlook.com>
2016-04-26 11:49:37 +0800

    tmp Tu2 26 Apr, 11:49

--- PhloxAR/image.py
+++ PhloxAR/image.py
@@ -1,4 +1,116 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import division, print_function
+from __future__ import unicode_literals, absolute_import
 from PhloxAR.base import *
 from PhloxAR.color import *
+from PhloxAR.linescan import *
+from numpy import int32
+from numpy import uint8
+
+try:
+    import urllib2
+except ImportError:
+    pass
+else:
+    import urllib
+
+import cv2
+
+from PhloxAR.exif import *
+
+
+if not init_options_handler.headless:
+    import pygame as pg
+
+
+import scipy.ndimage as ndimage
+import scipy.stats.stats as sci_stats
+import scipy.cluster.vq as sci_cvq
+import scipy.linalg as linalg
+import copy
+
+
+def enum(*seq):
+    n = 0
+    enums = {}
+    if isinstance(seq, dict):
+        enums = seq
+    else:
+        for elem in seq:
+            enums[elem] = n
+            n += 1
+    return type('Enum', (), enums)
+
+ColorSpace = enum('UNKNOWN', 'BGR', 'GRAY', 'RGB', 'HLS', 'HSV', 'XYZ', 'YCrCb')
+
+
+class Image(object):
+    """
+    The Image class allows you to convert to and from a number of source types
+    with ease. It also has intelligent buffer management, so that modified
+    copies of the Image required for algorithms such as edge detection, etc can
+    be cached an reused when appropriate.
+
+    Image are converted into 8-bit, 3-channel images in RGB color space. It
+    will automatically handle conversion from other representations into this
+    standard format. If dimensions are passed, an empty image is created.
+    """
+    width = 0
+    height = 0
+    depth = 0
+    name = ''
+    handle = ''
+    camera = ''
+
+    _layers = []
+    _do_hue_palette = False
+    _palette_bins = None
+    _palette = None
+    _palette_members = None
+    _palette_percentages = None
+
+    _barcode_reader = ''  # property for the ZXing barcode reader
+
+    # these are buffer frames for various operations on the image
+    _bitmap = ''  # the bitmap (iplimage) representation of the image
+    _matrix = ''  # the matrix (cvmat) representation
+    _gray_matrix = ''  # the gray scale (cvmat) representation
+    _equalize_gray_bitmap = ''  # the normalized bitmap
+    _blob_label = ''  # the label image for blobbing
+    _edge_map = ''  # holding reference for edge map
+    _canny_param = ''  # parameters that created _edge_map
+    _pil = ''  # holds a PIL object in buffer
+    _numpy = ''  # numpy form buffer
+    _gray_numpy = ''  # gray scale numpy for key point stuff
+    _color_space = ColorSpace.UNKNOWN
+
+
+
+class ImageSet(list):
+    """
+    An abstract class for keeping a list of images.
+    """
+    file_list = None
+
+    def __init__(self, directory=None):
+        if not directory:
+            return
+
+        if isinstance(directory, list):
+            if isinstance(directory[0], Image):
+                super(ImageSet, self).__init__(directory)
+            elif isinstance(directory[0], str):
+                super(ImageSet, self).__init__(map(Image, directory))
+        elif directory.lower() == 'samples' or directory.lower == 'sample':
+            path = LAUNCH_PATH
+            path = os.path.realpath(path)
+            directory = os.path.join(path, 'sample_images')
+            self.load(directory)
+        else:
+            self.load(directory)
+
+    # TODO: implementation
+    def load(self, directory):
+        pass
