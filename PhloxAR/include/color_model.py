85325eacee9708a4ae39f18076eab1443148ef49
Matthias Chen <matthias_cy@outlook.com>
2016-05-03 09:40:02 +0800

    color_model funcs

--- PhloxAR/include/color_model.py
+++ PhloxAR/include/color_model.py
@@ -18,9 +18,41 @@ class ColorModel(object):
     """
     # TODO: discretize the color space into smaller intervals
     # TODO: work in HSV space
-    is_background = True
-    data = {}
-    bits = 1
+    _is_bkg = True
+    _data = {}
+    _bits = 1
 
-    def __init__(self, data=None):
+    def __init__(self, data=None, is_bkg=True):
+        self._is_bkg = is_bkg
+        self._data = data
+        self._bits = 1
+
+        if data:
+            try:
+                [self.add(d) for d in data]
+            except TypeError:
+                self.add(data)
+
+    def _make_canonical(self, data):
+        pass
+
+    def reset(self):
+        pass
+
+    def add(self, data):
+        pass
+
+    def threshold(self, image):
+        pass
+
+    def contains(self, c):
+        pass
+
+    def set_is_bkg(self):
+        pass
+
+    def load(self, filename):
+        pass
+
+    def save(self, filename):
         pass
