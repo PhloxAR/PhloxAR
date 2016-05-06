7572d5fd8fbf928d66143edf64634ce29570e763
Matthias Chen <matthias_cy@outlook.com>
2016-05-06 18:59:06 +0800

    Vector2, Vector3

--- PhloxAR/loader/math3d.py
+++ PhloxAR/loader/math3d.py
@@ -26,14 +26,6 @@ import operator
 import types
 
 
-class Point2(object):
-    pass
-
-
-class Point3(object):
-    pass
-
-
 class Vector2(object):
     def __init__(self, x=0, y=0):
         self._x = x
@@ -49,7 +41,7 @@ class Vector2(object):
 
     def __eq__(self, other):
         if isinstance(other, Vector2):
-            return self._x == other._x and self._y == other._y
+            return self._x == other.x and self._y == other.y
 
     def __ne__(self, other):
         return not self.__eq__(other)
@@ -90,7 +82,134 @@ class Vector2(object):
                 raise AttributeError(key)
 
     def __add__(self, other):
-        pass
+        if isinstance(other, Vector2):
+            if self.__class__ is other.__class__:
+                _class = Vector2
+            else:
+                _class = Point2
+
+            return _class(self._x + other.x, self._y + other.y)
+        else:
+            assert hasattr(other, '__len__') and len(other) == 2
+            return Vector2(self._x + other[0], self._y + other[1])
+
+    __radd__ = __add__
+
+    def __iadd__(self, other):
+        if isinstance(other, Vector2):
+            self._x += other.x
+            self._y += other.y
+        else:
+            self.x += other[0]
+            self.y += other[1]
+
+        return self
+
+    def __sub__(self, other):
+        if isinstance(object, Vector2):
+            if self.__class__ is other.__class__:
+                _class = Vector2
+            else:
+                _class = Point2
+            return _class(self._x - other.x, self._y - other.y)
+        else:
+            assert hasattr(other, '__len__') and len(other) == 2
+            return Vector2(self._x - other[0], self._y - other[1])
+
+    def __rsub__(self, other):
+        if isinstance(other, Vector2):
+            return Vector2(other.x - self._x, other.y - self._y)
+
+        else:
+            assert hasattr(other, '__len__') and len(other) == 2
+            return Vector2(other.x - self[0], other.y - self[1])
+
+    def __mul__(self, other):
+        assert type(other) in (int, long, float)
+        return Vector2(self._x * other, self._y * other)
+
+    __rmul__ = __mul__
+
+    def __imul__(self, other):
+        assert type(other) in (int, long, float)
+        self._x *= other
+        self._y *= other
+
+        return self
+
+    def __div__(self, other):
+        assert type(other) in (int, long, float)
+        return Vector2(operator.div(self._x, other), operator.div(self._y, other))
+
+    def __rdiv__(self, other):
+        assert type(other) in (int, long, float)
+        return Vector2(operator.div(other, self._x), operator.div(other, self._y))
+
+    def __floordiv__(self, other):
+        assert type(other) in (int, long, float)
+        return Vector2(operator.floordiv(self._x, other),
+                       operator.floordiv(self._y, other))
+
+    def __rfloordiv__(self, other):
+        assert type(other) in (int, long, float)
+        return Vector2(operator.floordiv(other, self._x),
+                       operator.floordiv(other, self._y))
+
+    def __truediv__(self, other):
+        assert type(other) in (int, long, float)
+        return Vector2(operator.truediv(self._x, other),
+                       operator.truediv(self._y, other))
+
+    def __rtruediv__(self, other):
+        assert type(other) in (int, long, float)
+        return Vector2(operator.truediv(other, self._x),
+                       operator.truediv(other, self._y))
+
+    def __neg__(self):
+        return Vector2(-self._x, -self._y)
+
+    __pos__ == __copy__
+
+    @property
+    def magnitude(self):
+        return math.sqrt(self._x**2 + self._y**2)
+
+    def magnitude_squared(self):
+        return self._x**2 + self._y**2
+
+    def normalize(self):
+        d = self.magnitude
+        if d:
+            self._x /= d
+            self._y /= d
+        return self
+
+    def normalized(self):
+        d = self.magnitude
+        if d:
+            return Vector2(self._x / d, self._y / d)
+        return self.copy()
+
+    def dot(self, other):
+        assert isinstance(other, Vector2)
+        return self._x * other.x + self._y * other.y
+
+    # to improve
+    def cross(self):
+        return Vector2(self._y, -self._x)
+
+    def reflect(self, normal):
+        # assume normal is normalized
+        assert isinstance(normal, Vector2)
+        d = 2 * (self.x * normal.x + self.y * normal.y)
+        return Vector2(self.x - d * normal.x, self.y - d * normal.y)
+
+    def angle(self, other):
+        return math.acos(self.dot(other) / self.magnitude)
+
+    def project(self, other):
+        n = other.normalized()
+        return self.dot(n) * n
 
     @property
     def x(self):
@@ -110,7 +229,257 @@ class Vector2(object):
 
 
 class Vector3(object):
-    pass
+    def __init__(self, x=0, y=0, z=0):
+        self._x = x
+        self._y = y
+        self._z = z
+
+    def __copy__(self):
+        return self.__class__(self._x, self._y, self._z)
+
+    copy = __copy__
+
+    def __repr__(self):
+        return '<Vector3 (%.2f, %.2f, %.2f)>' % (self._x, self._y, self._z)
+
+    def __eq__(self, other):
+        if isinstance(other, Vector3):
+            return (self._x == other.x and self.y == other.y and
+                    self.z == other.z)
+        else:
+            assert hasattr(other, '__len__') and len(other) == 3
+            return (self.x == other[0] and self.y == other[1] and
+                   self.z == other[2])
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __nonzero__(self):
+        return self._x !=0 or self._y != 0 or self._z != 0
+
+    def __len__(self):
+        return 3
+
+    def __getitem__(self, key):
+        return (self._x, self._y, self._z)[key]
+
+    def __setitem__(self, key, value):
+        l = [self._x, self._y, self._z]
+        l[key] = value
+        self.x, self.y, self.z = l
+
+    def __iter__(self):
+        return iter((self._x, self._y, self._z))
+
+    def __getattr__(self, item):
+        try:
+            return tuple([(self._x, self._y, self._z)['xyz'.index(c)] for c in item])
+        except ValueError:
+            raise AttributeError(item)
+
+    def __setattr__(self, key, value):
+        if len(key) == 1:
+            object.__setattr__(self, key, value)
+        else:
+            try:
+                l = [self._x, self._y, self._z]
+                for k, v in map(None, key, value):
+                    l['xyz'.index(k)] = v
+                self._x, self._y, self._z = l
+            except ValueError:
+                raise AttributeError(key)
+
+    def __add__(self, other):
+        if isinstance(other, Vector3):
+            if self.__class__ is other.__class__:
+                _class = Vector3
+            else:
+                _class = Point3
+            return _class(self._x + other.x, self._y + other.y, self._z + other.z)
+        else:
+            assert hasattr(other, '__len__') and len(other) == 3
+        return Vector3(self._x + other[0], self._y + other[1], self._z + other[2])
+
+    def __iadd__(self, other):
+        if isinstance(other, Vector3):
+            self._x += other.x
+            self._y += other.y
+            self._z += other.z
+        else:
+            self._x += other[0]
+            self._y += other[1]
+            self._z += other[2]
+        return self
+
+    def __sub__(self, other):
+        if isinstance(other, Vector3):
+            if self.__class__ is other.__class__:
+                _class = Vector3
+            else:
+                _class = Point3
+            return Vector3(self._x - other.x, self._y - other.y, self._z - other.z)
+        else:
+            assert hasattr(other, '__len__') and len(other) == 3
+            return Vector3(self._x - other[0], self._y - other[1], self._z - other[2])
+
+    def __rsub__(self, other):
+        if isinstance(other, Vector3):
+            return Vector3(other.x - self._x, other.y - self._y, other.z - self._z)
+        else:
+            assert hasattr(other, '__len__') and len(other) == 3
+            return Vector3(other._x - self[0], other._y - self[1], other._z - self[2])
+
+    def __mul__(self, other):
+        if isinstance(other, Vector3):
+            # TODO component-wise mul/div in-place and on Vector2; docs.
+            if self.__class__ is Point3 or other.__class__ is Point3:
+                _class = Point3
+            else:
+                _class = Vector3
+            return _class(self._x * other.x, self._y * other.y, self._z * other.z)
+        else:
+            assert type(other) in (int, long, float)
+            return Vector3(self._x * other,
+                           self._y * other,
+                           self._z * other)
+
+    __rmul__ = __mul__
+
+    def __imul__(self, other):
+        assert type(other) in (int, long, float)
+        self._x *= other
+        self._y *= other
+        self._z *= other
+        return self
+
+    def __div__(self, other):
+        assert type(other) in (int, long, float)
+        return Vector3(operator.div(self._x, other), operator.div(self._y, other),
+                       operator.div(self._z, other))
+
+    def __rdiv__(self, other):
+        assert type(other) in (int, long, float)
+        return Vector3(operator.div(other, self._x), operator.div(other, self._y),
+                       operator.div(other, self._z))
+
+    def __floordiv__(self, other):
+        assert type(other) in (int, long, float)
+        return Vector3(operator.floordiv(self._x, other),
+                       operator.floordiv(self._y, other),
+                       operator.floordiv(self._z, other))
+
+    def __rfloordiv__(self, other):
+        assert type(other) in (int, long, float)
+        return Vector3(operator.floordiv(other, self._x),
+                       operator.floordiv(other, self._y),
+                       operator.floordiv(other, self._z))
+
+    def __truediv__(self, other):
+        assert type(other) in (int, long, float)
+        return Vector3(operator.truediv(self._x, other),
+                       operator.truediv(self._y, other),
+                       operator.truediv(self._z, other))
+
+    def __rtruediv__(self, other):
+        assert type(other) in (int, long, float)
+        return Vector3(operator.truediv(other, self._x),
+                       operator.truediv(other, self._y),
+                       operator.truediv(other, self._z))
+
+    def __neg__(self):
+        return Vector3(-self._x, -self._y, -self._z)
+
+    __pos__ == __copy__
+
+    @property
+    def magnitude(self):
+        return math.sqrt(self._x ** 2 + self._y ** 2 + self._z ** 2)
+
+    @property
+    def magnitude_squared(self):
+        return self._x ** 2 + self._y ** 2 + self._z ** 2
+
+    def normalize(self):
+        d = self.magnitude
+        if d:
+            self._x /= d
+            self._y /= d
+            self._z /=d
+        return self
+
+    def normalized(self):
+        d = self.magnitude
+        if d:
+            Vector3(self._x / d, self._y / d, self._z / d)
+        return self.copy()
+
+    def dot(self, other):
+        assert isinstance(other, Vector3)
+        return self._x * other.x + self._y * other.y + self._z * other.z
+
+    def cross(self, other):
+        assert isinstance(other, Vector3)
+        return Vector3(self._y * other.z - self._z * other.y,
+                       self._z * other.x - self._x * other.z,
+                       self._x * other.y - self._y * other.x)
+
+    def reflect(self, normal):
+        # assume normal is normalized
+        assert isinstance(normal, Vector3)
+        d = 2 * (self.x * normal.x + self.y * normal.y + self.z * normal.z)
+        return Vector3(self.x - d * normal.x,
+                       self.y - d * normal.y,
+                       self.z - d * normal.z)
+
+    def rotate_around(self, axis, theta):
+        """
+        Return the vector rotated around axis through angle theta. Right hand rule applies"""
+
+        # Adapted from equations published by Glenn Murray.
+        # http://inside.mines.edu/~gmurray/ArbitraryAxisRotation/ArbitraryAxisRotation.html
+        x, y, z = self._x, self._y, self._z
+        u, v, w = axis.x, axis.y, axis.z
+
+        # Extracted common factors for simplicity and efficiency
+        r2 = u ** 2 + v ** 2 + w ** 2
+        r = math.sqrt(r2)
+        ct = math.cos(theta)
+        st = math.sin(theta) / r
+        dt = (u * x + v * y + w * z) * (1 - ct) / r2
+        return Vector3((u * dt + x * ct + (-w * y + v * z) * st),
+                       (v * dt + y * ct + (w * x - u * z) * st),
+                       (w * dt + z * ct + (-v * x + u * y) * st))
+
+    def angle(self, other):
+        return math.acos(self.dot(other) / (self.magnitude * other.magnitude))
+
+    def project(self, other):
+        n = other.normalized()
+        return self.dot(n) * n
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, val):
+        self._x = val
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, val):
+        self._y = val
+
+    @property
+    def z(self):
+        return self._z
+
+    @z.setter
+    def z(self, val):
+        self._z = val
 
 
 class Matrix3(object):
@@ -126,6 +495,14 @@ class Quaternion(object):
 
 
 class Geometry(object):
+    pass
+
+
+class Point2(Vector2, Geometry):
+    pass
+
+
+class Point3(object):
     pass
 
 
@@ -163,3 +540,10 @@ class Sphere(object):
 
 class Plane(object):
     pass
+
+vec2 = Vector2
+vec3 = Vector3
+mat3 = Matrix3
+mat4 = Matrix4
+ray2 = Ray2
+ray3 = Ray3
