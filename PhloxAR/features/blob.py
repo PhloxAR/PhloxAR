66201e6d73a909bc0ad932ad4b5de9d2ce30d4fe
Matthias Chen <matthias_cy@outlook.com>
2016-05-08 12:31:29 +0800

    add Blob class

---
+++ PhloxAR/features/blob.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+from __future__ import division, print_function
+from __future__ import absolute_import, unicode_literals
+
+from PhloxAR.base import math
+from PhloxAR.base import sss
+from PhloxAR.base import *
+from PhloxAR.features.feature import  Feature
+from PhloxAR.color import Color
+from PhloxAR.image import Image
+
+
+class Blob(Feature):
+    pass
