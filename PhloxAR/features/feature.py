408fd0198df0d4ec89ad559f35ef4a73508a8e19
Matthias Chen <matthias_cy@outlook.com>
2016-05-08 12:04:42 +0800

    tmp commit

--- PhloxAR/features/feature.py
+++ PhloxAR/features/feature.py
@@ -24,6 +24,7 @@ class Feature(object):
     _min_y = None
     _width = None
     _height = None
+    _aspect_ratio = None
     _src_img_width = None
     _src_img_height = None
 
@@ -46,18 +47,22 @@ class Feature(object):
 
     @property
     def x(self):
+        self._update_extents()
         return self._x
 
     @property
     def y(self):
+        self._update_extents()
         return self._y
 
     @property
     def width(self):
+        self._update_extents()
         return self._width
 
     @property
     def height(self):
+        self._update_extents()
         return self._height
 
     def reassign(self, img):
@@ -105,88 +110,336 @@ class Feature(object):
         self._image.show()
 
     def distance_from(self, point=(-1, -1)):
-        pass
+        """
+        Given a point (default to center of the image), return the euclidean
+        :param point: (x, y) tuple on the image to measure distance from
+        :return: the distance as a floating point value in pixels.
+        """
+        if point[0] == -1 or point[1] == -1:
+            point = npy.array(self._image.size()) / 2
+
+        return spsd.euclidean(point, [self._x, self._y])
 
     def mean_color(self):
-        pass
+        """
+        Return the average color within the feature as a tuple.
+        :return: an RGB color tuple
+        """
+        return self._image[self._x, self._y]
 
     def color_distance(self, color=(0, 0, 0)):
-        pass
+        """
+        Return the euclidean color distance of the color tuple at x, y from
+        a given color (default black).
+        :param color: a RGB tuple to calculate from which to calculate the
+                       color distance.
+        :return: a floating point color distance value.
+        """
+        return spsd.euclidean(npy.array(color), npy.array(self.mean_color()))
 
     def angle(self):
-        pass
+        """
+        Return the angle (theta) in degrees of the feature. The default is 0
+        (horizontal). Not valid for all features.
+        :return:
+        """
+        return 0
 
     def length(self):
-        pass
+        """
+        Returns the longest dimension of the feature (i.e., max(width, height))
+        :return: a floating point length value.
+        """
+        return float(npy.max([self.width, self.height]))
 
     def distance_to_nearest_edge(self):
-        pass
+        """
+        Returns the distance, in pixels, from the nearest image edge.
+        :return: integer distance to the nearest edge.
+        """
+        w = self._image.width
+        h = self._image.height
+
+        return npy.min([self._min_x, self._min_y,
+                        w - self._max_x, h - self._max_y])
 
     def on_image_edge(self, tolerance=1):
-        pass
+        """
+        Returns True if the feature is less than 'tolerance' pixels away from
+        the nearest edge.
+        :param tolerance: the distance in pixels at which a feature qualifies
+                           as being on the image edge.
+        :return: True if the feature is on the edge, False otherwise.
+        """
+        return self.distance_to_nearest_edge() <= tolerance
 
     def aspect_ratio(self):
-        pass
+        """
+        Return the aspect ratio of the feature, which for our purposes is
+        max(width, height) / min(width, height)
+        :return: a single floating point value of the aspect ratio.
+        """
+        self._update_extents()
+        return self._aspect_ratio
 
     def area(self):
-        pass
+        """
+        Returns the area (number of pixels) covered by the feature
+        :return: an integer area of the feature
+        """
+        return self.width * self.height
 
+    @property
     def width(self):
-        pass
+        """
+        Returns the width of the feature.
+        :return: integer value for the feature's width
+        """
+        self._update_extents()
+        return self._width
 
+    @property
     def height(self):
-        pass
+        """
+        Returns the height of the feature.
+        :return: integer value for the feature's height
+        """
+        self._update_extents()
+        return self._height
 
     def crop(self):
-        pass
+        """
+        Crops the source image to the location of the feature and
+        returns a new Image.
+        :return: an Image that is cropped to the feature position and size.
+        """
+        return self._image.crop(self._x, self._y,
+                                self.width, self.height, centered=True)
 
     def __repr__(self):
-        pass
+        return "{}.{} at ({}, {})".format(self.__class__.__module__,
+                                          self.__class__.__name__,
+                                          self._x, self._y)
 
     def bbox(self):
-        pass
+        """
+        Returns a rectangle which bounds the blob.
+        :return: a list of [x, y, w, h] where (x, y) are the top left point
+                  of the rectangle and w, h are its width and height.
+        """
+        self._update_extents()
+        return self._bbox
 
     def extents(self):
-        pass
+        """
+        Returns the maximum and minimum x and y values for the feature and
+        returns them as a tuple
+        :return: a tuple of the extents of the feature.
+                  Order is(max_x, max_y, min_x, min_y)
+        """
+        self._update_extents()
+        return self._extents
 
     def _update_extents(self, new_feature=False):
-        pass
+        max_x = self._max_x
+        max_y = self._max_y
+        min_x = self._min_y
+        min_y = self._min_y
+        width = self._width
+        height = self._height
+        extents = self._extents
+        bbox = self._bbox
+
+        if new_feature or None in [max_x, min_x, max_y, min_y,
+                                   width, height, extents, bbox]:
+
+            max_x = max_y = float("-infinity")
+            min_x = min_y = float("infinity")
+
+            for p in self._points:
+                if p[0] > max_x:
+                    max_x = p[0]
+                if p[0] < min_x:
+                    min_x = p[0]
+                if p[1] > max_y:
+                    max_y = p[1]
+                if p[1] < min_y:
+                    min_y = p[1]
+
+            width = max_x - min_x
+            height = max_y - min_y
+
+            if width <= 0:
+                width = 1
+
+            if height <= 0:
+                height = 1
+
+            self._bbox = [min_x, min_y, width, height]
+            self._extents = [max_x, min_x, max_y, min_y]
+
+            if width > height:
+                self._aspect_ratio = float(width / height)
+            else:
+                self._aspect_ratio = float(height / width)
+
+            self._max_x = max_x
+            self._min_x = min_x
+            self._max_y = max_y
+            self._min_y = min_y
+            self._width = width
+            self._height = height
 
     def min_x(self):
-        pass
+        """
+        Returns the minimum x value of the bounding box of the feature.
+        :return: an integer value of the minimum x value of the feature.
+        """
+        self._update_extents()
+        return self._min_x
 
     def min_y(self):
-        pass
+        """
+        Returns the minimum y value of the bounding box of the feature.
+        :return: an integer value of the minimum y value of the feature.
+        """
+        self._update_extents()
+        return self._min_y
 
     def max_x(self):
-        pass
+        """
+        Returns the maximum x value of the bounding box of the feature.
+        :return: an integer value of the maximum x value of the feature.
+        """
+        self._update_extents()
+        return self._max_x
 
     def max_y(self):
-        pass
+        """
+        Returns the maximum y value of the bounding box of the feature.
+        :return: an integer value of the maximum y value of the feature.
+        """
+        self._update_extents()
+        return self._max_y
 
     def top_left_corner(self):
-        pass
+        """
+        Returns the top left corner of the bounding box of the blob as tuple
+        :return: a tuple of the top left corner
+        """
+        self._update_extents()
+        return self._min_x, self._min_y
 
     def bottom_right_corner(self):
-        pass
+        """
+        Returns the bottom right corner of the bounding box of the blob as tuple
+        :return: a tuple of the bottom right corner
+        """
+        self._update_extents()
+        return self._max_x, self._max_y
 
-    def bottomLeftCorner(self):
-        pass
+    def bottom_left_corner(self):
+        """
+        Returns the bottom left corner of the bounding box of the blob as tuple
+        :return: a tuple of the bottom left corner
+        """
+        self._update_extents()
+        return self._min_x, self._max_y
 
-    def topRightCorner(self):
-        pass
+    def top_right_corner(self):
+        """
+        Returns the top right corner of the bounding box of the blob as tuple
+        :return: a tuple of the top right corner
+        """
+        self._update_extents()
+        return self._max_x, self._min_y
 
-    def above(self, object):
-        pass
+    def above(self, obj):
+        """
+        Return True if the feature is above the object, where object can be
+        a bounding box, bounding circle, a list of tuples in a closed polygon,
+        or any other features.
+        :param obj: bounding box - (x, y, w, h) where (x, y) is the top left
+                    bounding circle - (x, y, r)
+                    a list of (x, y) tuples defining a closed polygon
+                    any two dimensional feature (e.g. blobs, circle ...
+        :return: Bool, True if the feature is above the object, False otherwise.
+        """
+        if isinstance(obj, Feature):
+            return self.max_y() < obj.min_y()
+        elif isinstance(obj, tuple) or isinstance(obj, npy.ndarray):
+            return self.max_y() < obj[1]
+        elif isinstance(obj, float) or isinstance(obj, int):
+            return self.max_y() < obj
+        else:
+            logger.warning("Did not recognize.")
+            return None
 
-    def below(self, object):
-        pass
+    def below(self, obj):
+        """
+        Return True if the feature is below the object, where object can be
+        a bounding box, bounding circle, a list of tuples in a closed polygon,
+        or any other features.
+        :param obj: bounding box - (x, y, w, h) where (x, y) is the top left
+                    bounding circle - (x, y, r)
+                    a list of (x, y) tuples defining a closed polygon
+                    any two dimensional feature (e.g. blobs, circle ...
+        :return: Bool, True if the feature is below the object, False otherwise.
+        """
 
-    def right(self, object):
-        pass
+        if isinstance(obj, Feature):
+            return self.min_y() > obj.max_y()
+        elif isinstance(obj, tuple) or isinstance(obj, npy.ndarray):
+            return self.min_y() > obj[1]
+        elif isinstance(obj, float) or isinstance(obj, int):
+            return self.min_y() > obj
+        else:
+            logger.warning("Did not recognize.")
+            return None
 
-    def left(self, object):
-        pass
+    def right(self, obj):
+        """
+        Return True if the feature is to the right object, where object can be
+        a bounding box, bounding circle, a list of tuples in a closed polygon,
+        or any other features.
+        :param obj: bounding box - (x, y, w, h) where (x, y) is the top left
+                    bounding circle - (x, y, r)
+                    a list of (x, y) tuples defining a closed polygon
+                    any two dimensional feature (e.g. blobs, circle ...
+        :return: Bool, True if the feature is to the right object, False otherwise.
+        """
+
+        if isinstance(obj, Feature):
+            return self.min_x() > obj.max_x()
+        elif isinstance(obj, tuple) or isinstance(obj, npy.ndarray):
+            return self.min_x() > obj[0]
+        elif isinstance(obj, float) or isinstance(obj, int):
+            return self.min_x() > obj
+        else:
+            logger.warning("Did not recognize.")
+            return None
+
+    def left(self, obj):
+        """
+        Return True if the feature is to the left object, where object can be
+        a bounding box, bounding circle, a list of tuples in a closed polygon,
+        or any other features.
+        :param obj: bounding box - (x, y, w, h) where (x, y) is the top left
+                     bounding circle - (x, y, r)
+                     a list of (x, y) tuples defining a closed polygon
+                     any two dimensional feature (e.g. blobs, circle ...
+        :return: Bool, True if the feature is to the left object, False otherwise.
+        """
+
+        if isinstance(obj, Feature):
+            return self.max_x() < obj.min_x()
+        elif isinstance(obj, tuple) or isinstance(obj, npy.ndarray):
+            return self.max_x() < obj[0]
+        elif isinstance(obj, float) or isinstance(obj, int):
+            return self.max_x() < obj
+        else:
+            logger.warning("Did not recognize.")
+            return None
 
     def contains(self, other):
         pass
@@ -200,5 +453,31 @@ class Feature(object):
     def _point_inside_polygon(self, point, polygon):
         pass
 
-    def bcircle(self):
-        pass
+    def bcir(self):
+        """
+        Calculates the minimum bounding circle of the blob in the image as
+        a (x, y, r) tuple.
+        :return: a (x, y, r) tuple where (x, y) is the center of the circle
+                  and r is the radius
+        """
+        try:
+            import cv2
+        except:
+            logger.warning("Unable to import cv2")
+            return None
+
+        contour = self.contour()
+
+        points = []
+
+        for pair in contour:
+            points.append([pair[0], pair[1]])
+
+        points = npy.array(points)
+
+        cen, rad = cv2.minEnclosingCircle(points)
+
+        return cen[0], cen[1], rad
+
+    def contour(self):
+        return []
