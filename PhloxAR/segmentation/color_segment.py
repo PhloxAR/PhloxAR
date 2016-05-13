682222d0557aab6661b0da2c5a7731770f7a31bc
Matthias Chen <matthias_cy@outlook.com>
2016-05-13 23:18:08 +0800

    ColorSegmentation declare

---
+++ PhloxAR/segmentation/color_segment.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function
+from __future__ import absolute_import, unicode_literals
+
+from PhloxAR.base import *
+from PhloxAR.features import Feature, FeatureSet, BlobMaker
+from PhloxAR.color_model import ColorModel
+from PhloxAR.color import Color
+from PhloxAR.image import Image
+from PhloxAR.segmentation.base import SegmentationBase
+
+
+class ColorSegmentation(SegmentationBase):
+    pass
