d134facc211196e11b3698f9bb27134a1b221b95
Matthias Chen <matthias_cy@outlook.com>
2016-05-15 22:50:15 +0800

    base.py rename -> segmentation_base.py

--- PhloxAR/segmentation/color_segment.py
+++ PhloxAR/segmentation/color_segment.py
@@ -3,11 +3,12 @@ from __future__ import division, print_function
 from __future__ import absolute_import, unicode_literals
 
 from PhloxAR.base import *
-from PhloxAR.features import Feature, FeatureSet, BlobMaker
+from PhloxAR.features.feature import Feature, FeatureSet
+from PhloxAR.features.blob_maker import BlobMaker
 from PhloxAR.color_model import ColorModel
 from PhloxAR.color import Color
 from PhloxAR.image import Image
-from PhloxAR.segmentation.base import SegmentationBase
+from PhloxAR.segmentation.segmentation_base import SegmentationBase
 
 
 class ColorSegmentation(SegmentationBase):
