57394b2146f549efa50dc1c401addd257237901b
Matthias Chen <matthias_cy@outlook.com>
2016-05-16 22:48:28 +0800

    lkt_tracker

--- PhloxAR/tracking/camshift_tracker.py
+++ PhloxAR/tracking/camshift_tracker.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function
+from __future__ import absolute_import, unicode_literals
+
 from PhloxAR.base import npy
 from PhloxAR.tracking.track import CAMShiftTrack
 
