930d29f78a1bf78fdfd8063e880e17c9e47326f3
Matthias Chen <matthias_cy@outlook.com>
2016-05-17 22:15:49 +0800

    basically finished but all untested

--- PhloxAR/__init__.py
+++ PhloxAR/__init__.py
@@ -1,1 +1,13 @@
-
+# -*- coding: utf-8 -*-
+from .version import __version__
+from .tracking import *
+from .segmentation import *
+from .dft import *
+from .color import *
+from .image import *
+from .camera import *
+from .color_model import *
+from .dc1394 import *
+from .math3 import *
+from .linescan import *
+from .stream import *
